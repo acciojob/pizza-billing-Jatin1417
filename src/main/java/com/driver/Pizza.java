111d8a2603af8ec3bf6f7ce11f80ad7defc3eabb
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-13 23:03:45 +0530

    second commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,27 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
-
+    private String bill="";
+    private boolean ischeeseadded = false;
+    private boolean istoppingsadded = false;
+    private boolean istakeaway = false;
+    private int toppingsprice;
+    private int cheeseprice = 80;
+    private int paperbag = 20;
+    private int veg = 300;
+    private int nonveg = 400;
+    private int initialprice;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg) {
+            this.price += veg;
+            initialprice = veg;
+        }
+        else{
+            this.price += nonveg;
+            initialprice = nonveg;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +33,44 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(ischeeseadded==false){
+        this.price+=80;
+        ischeeseadded=true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(istoppingsadded==false){
+            if(isVeg){
+                this.toppingsprice=70;
+                this.price+=this.toppingsprice;
+            }
+            else{
+                this.toppingsprice = 120;
+                price+=this.toppingsprice;
+            }
+        }
+        
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(istakeaway==false){
+            this.price+=this.paperbag;
+            istakeaway=true;
+        }
+       
     }
-
+  private boolean isbill=false;
     public String getBill(){
         // your code goes here
+        if(isbill==false){
+         bill+="Base Price Of The Pizza: "+ this.initialprice+"\n";
+         bill+="Extra Cheese Added: "+this.cheeseprice+"\n";
+         bill+="Extra Toppings Added: "+this.toppingsprice+"\n";
+         bill+="Paperbag Added: "+this.paperbag+"\n";
+         bill+="Total Price: "+this.price+"\n";
+          isbill = true;
+        }
         return this.bill;
     }
 }
